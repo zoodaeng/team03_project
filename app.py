7d014301a997aa98e620d1db8e8626e7b443df80
zoodaeng <ju4573@naver.com>
2025-03-31 18:05:49 +0900

    불필요한 페이지 제거 및 메일 제목 코드 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, render_template, request
 import os
 import send_alert_email, privacy_detector
 
@@ -11,24 +11,6 @@ UPLOAD_PATH = 'uploads'
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-#파일 업로드 겁사
-@app.route('/upload', methods=['POST'])
-def upload():
-    if "file" not in request.files:
-        return "파일이 없습니다.", 400
-    
-    file = request.files["file"]
-    if file.filename == "":
-        return "파일이 선택되지 않았습니다.", 400
-    
-    #파일 저장
-    file_path = os.path.join(UPLOAD_PATH, file.filename)
-    file.save(file_path)
-    print(file_path)
-    
-    #업로드 후 결과 페이지로 리다이렉트
 
 
 #결과 페이지
@@ -51,7 +33,7 @@ def result():
     print(result)
 
     #결과 메일로 전송
-    send_alert_email.send_alert_email(file.name, result)
+    send_alert_email.send_alert_email(file.filename, result)
 
     return render_template("result.html")
 
